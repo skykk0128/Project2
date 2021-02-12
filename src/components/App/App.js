39eb30d46d81877d441a274ba8a5debe2954b109
Steven Wang <skykk0128@gmail.com>
2021-02-13 00:30:37 +1100

    Create basic structure of weather app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,23 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
 
-const App = () => <div>Hello</div>;
+const App = () => (
+  <div className="app">
+    <div className="app__container">
+      <div className="app__container__top">
+        <div className="temperaturebanner"></div>
+        <div className="otherfeatures"></div>
+      </div>
+      <div className="app__container__bottom">
+        <div className="othercities">
+            <h2>Other Cities</h2>
+        </div>
+        <div className="forecast">
+          <h2>Forecast</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
