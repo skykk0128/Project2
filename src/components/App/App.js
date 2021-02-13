87bffc95ac2c9daaae9954d41d6c9da90f9c0c39
Steven Wang <skykk0128@gmail.com>
2021-02-13 16:39:45 +1100

    New changes made to Project 2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,52 @@
 import React from "react";
+import cloudy from "./assets/svgs/cloudy.svg";
 import "./App.css";
 
 const App = () => (
   <div className="app">
+    <h1>Weather App</h1>
     <div className="app__container">
       <div className="app__container__top">
-        <div className="temperaturebanner"></div>
-        <div className="otherfeatures"></div>
+        <div className="temperaturebanner">
+          <div className="temperaturebanner__temperature">
+            <div className="degree">
+              <div className="degree__figure">17</div>
+              <div className="degree__symbol"></div>
+              <div className="degree__celsius degree__measure">C</div>
+              {/* <div className="degree__seperator">/</div>
+              <div className="degree__figure">62.6</div>
+              <div className="degree__symbol"></div>
+              <div className="degree__fahrenheit degree__measure">F</div> */}
+            </div>
+            <div className="weather">Cloudy</div>
+          </div>
+          <div className="temperaturebanner__city">Melbourne</div>
+        </div>
+        <div className="otherfeatures">
+          <div className="feature__humidity">
+            <div className="feature__name">HUMIDITY</div>
+            <div className="feature__figure">57%</div>
+          </div>
+          <div className="feature__wind">
+            <div className="feature__name">WIND</div>
+            <div className="feature__figure">s 21 km/h</div>
+          </div>
+        </div>
       </div>
       <div className="app__container__bottom">
         <div className="othercities">
-            <h2>Other Cities</h2>
+          <h2>Other Cities</h2>
+          <div className="othercities__singlelocation">
+            <div className="othercities__city">Sydney</div>
+            <div className="othercities__degree">
+              <div className="citydegree__figure">19</div>
+              <div className="citydegree__symbol"></div>
+              <div className="citydegree__measure">C</div>
+            </div>
+            <div className="othercities__weather">
+              <img src={cloudy} alt="cloudy"></img>
+            </div>
+          </div>
         </div>
         <div className="forecast">
           <h2>Forecast</h2>
